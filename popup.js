a72586eda9938550e31d49f2f772a04ee42dbcbf
racefn <jokesjosh@gmail.com>
2020-11-07 19:17:22 -0500

    more css artwork

--- popup.js
+++ popup.js
@@ -17,13 +17,12 @@ chrome.storage.local.get(days[day - 1], (value) => {
 
   for (var course of dayArray) {
     const button = document.createElement('BUTTON');
-    button.setAttribute('type', 'submit');
+    button.setAttribute('type', 'sumbit');
+    button.setAttribute('id','stylingButton')
 
-    const name = document.createTextNode(course.className);
-    const time = document.createTextNode(course.lectureTime);
+    const title = document.createTextNode(course.className + " @ " + course.lectureTime);
 
-    button.appendChild(name);
-    button.appendChild(time);
+    button.appendChild(title);
     document.body.appendChild(button);
 
     button.onclick = function () {
